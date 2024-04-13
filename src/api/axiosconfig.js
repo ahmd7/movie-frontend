ce770f22304cfe8ca238175a7f7655e1a3cb07e5
Ahmed Samir <134155493+ahmd7@users.noreply.github.com>
2024-04-13 05:01:35 +0200

    Update axiosconfig.js

--- src/api/axiosconfig.js
+++ src/api/axiosconfig.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 export default axios.create({
-    baseURL: http://backend:8080,
+    baseURL: http://localhost:8080,
     headers: { "Access-Control-Allow-Origin": "*"}
 })
