59a3bf82e2d254c4ed12924047a3906afde515d4
Ahmed Samir <134155493+ahmd7@users.noreply.github.com>
2024-04-21 23:13:30 +0200

    Update axiosconfig.js

--- src/api/axiosconfig.js
+++ src/api/axiosconfig.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
+const baseURL = process.env.BACKEND ? `http://${process.env.BACKEND}:8080` : 'http://localhost:8080';
+
 export default axios.create({
-    baseURL: process.env.BACKEND:8080,
+    baseURL,
     headers: {"ngrok-skip-browser-warning": "true"}
 });
